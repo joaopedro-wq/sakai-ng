8da6fcae84581c357f203927289924c5324cfde8
joaopedro-wq <joao.bandeiraoliveira@gmail.com>
2025-02-01 21:04:57 -0300

    refactore: correcoes commit anterior

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
-# requisitos
+## Front-end
+Projeto sobre registro nutricional de alimentos. Crud onde pode cadastrar, editar e excluir alimentos, cadastrar seu registro usuario, tela de login.
 
-Angular CLI version 13.0.4.
-node version 18 ou superior
+## Back-end
+rotas e tabelas criadas utilizando laravel 11, sistema de autenticacao com token, sistema de exportaçao de planilhas e a base do crud para suportar o front-end
 
-## Development server
+## Tecnologias 
+Projeto em desenvolvimento utilizando tecnlogias PHP Laravel 11 para o back-end, e para o front template do primeNG utilizando Angular 17, para o banco utilizando MYSQL com fork mariaDB.
 
-ng server
-build: ng build
+## Requisitos Minimos
+PHP version 8.2
+Composer version 2.6
+Node Version 18.2
+MariaDB Version 10.4.
 
-## instalar depedencia
+## Como utilizar back-end
+CLonar o repositorio do back-end (Vitality-Back) e utilizar comando 'composer install'
+Para criação das tabelas 'php artisan migrate'
+para rodar aplicacao: 'php artisan serve' 
 
-npm install
-
+## como utilizar front-end
+CLonar repositorio (sakai) e utilizar comando 'npm install'
+rodar aplicacao: 'ng s'
+buildar aplicacao: 'ng build'
--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -17,24 +17,36 @@ export class AppMenuComponent implements OnInit {
             {
                 label: 'Home',
                 items: [
-                    {
-                        label: 'Dashboard',
-                        icon: 'pi pi-fw pi-home',
-                        routerLink: ['/'],
-                    },
-                ],
+                    { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] }
+                ]
             },
-
             {
-                label: 'Usuarios',
+                label: 'Cadastro',
                 items: [
-                    {
-                        label: 'Usuarios',
-                        icon: 'pi pi-fw pi-users',
-                        routerLink: ['/'],
-                    },
-                ],
+                    { label: 'Refeição', icon: 'pi pi-server', routerLink: ['/refeicoes'] },
+                    { label: 'Dieta', icon: 'pi pi-file', routerLink: ['/dietas'] },
+                    { label: 'Meta Diária', icon: 'pi  pi-chart-line', routerLink: ['/metas'] },
+
+
+                ]
             },
+            {
+                label: 'Registro',
+                items: [
+                    { label: 'Alimentos', icon: 'pi pi-heart', routerLink: ['/alimentos'] },
+                    { label: 'Registro Diário', icon: 'pi pi-bars', routerLink: ['/registros'] },
+
+
+                ]
+            },
+            {
+                label: 'Estatística',
+                items: [
+                    { label: 'Estatística Alimentação', icon: 'pi pi-chart-bar', routerLink: ['/relatorio'] },
+                    
+                ]
+            },
+            
         ];
     }
 }
